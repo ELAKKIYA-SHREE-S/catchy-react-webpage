f7e33ca2829d3fce81ae7dc3d1ad29a8c940a735
ELAKKIYA SHREE S <elakkiyashree.s2021csea@sece.ac.in>
2023-10-28 11:06:47 +0530

    initial commit

---
+++ rrw/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
